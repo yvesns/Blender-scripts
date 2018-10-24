be945b8d38b0ca58667f3d84657670164c78f014
yvesns <yves.ns2@gmail.com>
2018-10-24 11:17:18 -0300

    Default camera configuration

--- SpriteCreator/SpriteCreator.py
+++ SpriteCreator/SpriteCreator.py
@@ -3,15 +3,23 @@ import math
 import numpy
 
 baseSpriteName = "Sprite"
+cameraBlendFile = "SpriteCreatorCamera.blend"
 
+#Circle settings
 circleName = "SpriteCreatorCircle"
-circleRadius = 10
-circleHeight = 10
+circleRadius = 25
+circleHeight = 12
 
+#Camera settings
 cameraName = "SpriteCreatorCamera"
-cameraXRotation = math.radians(72)
+cameraXRotation = math.radians(73)
 
+importCameraSetup = True
+importAppending = True
 createCameraSetup = True
+
+setupRender = True
+mirrorSprites = True
 
 def start():
 	circle = bpy.data.objects.get(circleName)
@@ -23,9 +31,13 @@ def start():
 			
 		if camera is None:
 			camera = createCamera(circle)
+	
+	#This freezes Blender until the render is done
+	bpy.ops.render.render(write_still = False)
 		
 def createCircle():
 	bpy.ops.mesh.primitive_circle_add(radius=circleRadius, fill_type='NOTHING', location=[0, 0, circleHeight])
+	bpy.context.active_object.name = circleName
 	
 	return bpy.context.active_object
 
@@ -33,7 +45,13 @@ def createCamera(circle):
 	bpy.ops.object.camera_add(view_align = False, location = [0, -circleRadius, circle.location.z], rotation = [cameraXRotation, 0, 0])
 	camera = bpy.context.active_object
 	camera.name = cameraName
-
-	camera.parent_set(circle)
+	
+	circle.select = True
+	camera.select = True
+	bpy.context.scene.objects.active = circle
+	bpy.ops.object.parent_set()
+	
+	camera.data.type = "ORTHO"
+	camera.data.ortho_scale = 10
 	
 start()

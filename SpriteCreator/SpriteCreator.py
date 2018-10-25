b858e7777039ac1982cb36b92e87b7e330719e9e
yvesns <yves.ns2@gmail.com>
2018-10-25 10:46:48 -0300

    Sprite creation

--- SpriteCreator/SpriteCreator.py
+++ SpriteCreator/SpriteCreator.py
@@ -2,6 +2,7 @@ import bpy
 import math
 import numpy
 
+outputDirectory = bpy.data.scenes["Scene"].render.filepath
 baseSpriteName = "Sprite"
 cameraBlendFile = "SpriteCreatorCamera.blend"
 
@@ -13,6 +14,20 @@ circleHeight = 12
 #Camera settings
 cameraName = "SpriteCreatorCamera"
 cameraXRotation = math.radians(73)
+
+directionsRight = {
+	"S": math.radians(0),
+	"SE": math.radians(-45),
+	"E": math.radians(-90),
+	"NE": math.radians(-135),
+	"N": math.radians(180)
+}
+
+directionsLeft = {
+	"SW": math.radians(45),
+	"W": math.radians(90),
+	"NW": math.radians(135)
+}
 
 importCameraSetup = True
 importAppending = True
@@ -31,9 +46,17 @@ def start():
 			
 		if camera is None:
 			camera = createCamera(circle)
+			
+	for direction, rotation in directionsRight.items():
+		circle.rotation_euler = (0, 0, rotation)
+		bpy.data.scenes["Scene"].render.filepath = outputDirectory + baseSpriteName + direction
+		bpy.ops.render.render(write_still = True)
 	
-	#This freezes Blender until the render is done
-	bpy.ops.render.render(write_still = False)
+	if not mirrorSprites:
+		for direction, rotation in directionsLeft.items():
+			circle.rotation_euler = (0, 0, rotation)
+			bpy.data.scenes["Scene"].render.filepath = outputDirectory + baseSpriteName + direction
+			bpy.ops.render.render(write_still = True)
 		
 def createCircle():
 	bpy.ops.mesh.primitive_circle_add(radius=circleRadius, fill_type='NOTHING', location=[0, 0, circleHeight])
